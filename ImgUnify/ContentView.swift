0afb8dbf00cdaa05a55c0d04baf6710a1e895ee1
flofriday <flohacksfriday@gmail.com>
2023-03-24 00:02:09 +0100

    Initial Commit

---
+++ ImgUnify/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  ImgUnify
+//
+//  Created by flo on 24.03.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ImgUnify/ImgUnifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ImgUnifyApp.swift
+//  ImgUnify
+//
+//  Created by flo on 24.03.23.
+//
+
+import SwiftUI
+
+@main
+struct ImgUnifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
